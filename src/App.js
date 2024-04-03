deb4d35ff9c1e3da105ee946311345641d64e003
Ceciel Zhong <xizhong@umich.edu>
2024-04-02 22:01:29 -0400

    added function, need more styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import AddInventory from './components/addInventory';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Product Inventory created on Reactjs</h1>
-         
+        <AddInventory/>
       </header>
 
     </div>
